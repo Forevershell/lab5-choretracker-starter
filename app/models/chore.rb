d41a8e511c84328a55888bf839312866e373e904
Alex Chen <anc1@andrew.cmu.edu>
2021-02-05 16:48:12 -0800

    tests created children

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,17 @@
 class Chore < ApplicationRecord
+    # belongs_to :child
+	# belongs_to :task
+
+	# validates_date :due_on
+
+	# scope :by_task, ->{joins(:task).order('tasks.name')}
+	# scope :chronological, ->{order('due_on', 'completed')}
+	# scope :pending, ->{where(completed: false)}
+	# scope :done, ->{where(completed: true)}
+	# scope :upcoming, ->{where("due_on >= ?", Date.current)}
+	# scope :past, ->{where("due_on < ?", Date.current)}
+
+	# def status
+	# 	return self.completed ? "Completed" : "Pending"
+	# end
 end
