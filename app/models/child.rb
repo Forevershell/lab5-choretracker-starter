35081fa94704db02c4871a01880e5cefd606d75d
Alex Chen <anc1@andrew.cmu.edu>
2021-02-05 16:50:10 -0800

    children model created

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,13 +1,13 @@
 class Child < ApplicationRecord
-	# has_many :chores
-	# has_many :tasks, through: :chores
+	has_many :chores
+	has_many :tasks, through: :chores
 
-	# validates_presence_of :first_name, :last_name
+	validates_presence_of :first_name, :last_name
 
-	# scope :alphabetical, ->{order('last_name', 'first_name')}
-	# scope :active, ->{where(active: true)}
+	scope :alphabetical, ->{order('last_name', 'first_name')}
+	scope :active, ->{where(active: true)}
 
-	# def name
-	# 	"#{self.first_name} #{self.last_name}"
-	# end
+	def name
+		"#{self.first_name} #{self.last_name}"
+	end
 end
