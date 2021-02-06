ea9d20a46cbe757d5cad698c2b6da57a36ab5e80
Alex Chen <anc1@andrew.cmu.edu>
2021-02-05 17:10:30 -0800

    finished chore

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,16 +2,16 @@ class Chore < ApplicationRecord
     belongs_to :child
 	belongs_to :task
 
-	# validates_date :due_on
+	validates_date :due_on
 
-	# scope :by_task, ->{joins(:task).order('tasks.name')}
-	# scope :chronological, ->{order('due_on', 'completed')}
-	# scope :pending, ->{where(completed: false)}
-	# scope :done, ->{where(completed: true)}
-	# scope :upcoming, ->{where("due_on >= ?", Date.current)}
-	# scope :past, ->{where("due_on < ?", Date.current)}
+	scope :by_task, ->{joins(:task).order('tasks.name')}
+	scope :chronological, ->{order('due_on', 'completed')}
+	scope :pending, ->{where(completed: false)}
+	scope :done, ->{where(completed: true)}
+	scope :upcoming, ->{where("due_on >= ?", Date.current)}
+	scope :past, ->{where("due_on < ?", Date.current)}
 
-	# def status
-	# 	return self.completed ? "Completed" : "Pending"
-	# end
+	def status
+		return self.completed ? "Completed" : "Pending"
+	end
 end
