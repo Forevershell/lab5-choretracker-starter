efc253bd3a9354890eea503c91aa89bebf077c76
Alex Chen <anc1@andrew.cmu.edu>
2021-02-05 17:04:01 -0800

    tested tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ApplicationRecord
-	# has_many :chores
-	# has_many :children, through: :chores
+	has_many :chores
+	has_many :children, through: :chores
 
-	# validates_presence_of :name
-	# validates_numericality_of :points, :only_integer => true, :greater_than_or_equal_to => 0, :allow_blank => true
+	validates_presence_of :name
+	validates_numericality_of :points, :only_integer => true, :greater_than_or_equal_to => 0, :allow_blank => true
 
-	# scope :alphabetical, ->{order('name')}
-	# scope :active, ->{where(active: true)}
+	scope :alphabetical, ->{order('name')}
+	scope :active, ->{where(active: true)}
 end
